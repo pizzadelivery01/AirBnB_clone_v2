3031a3c2a950245ebe308f3e6b0a348de00d1fe4
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-06 21:24:32 -0400

    added flag

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,6 +15,6 @@ Holberton School
 </html>
 " | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "i 45 \location /hbnb_static {\n\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i " 45i \location /hbnb_static {\n\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
 sudo service nginx restart
