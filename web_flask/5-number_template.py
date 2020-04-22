f8b4f41bf2a6916bb3bf94c8b71306ca2ea1f024
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-21 21:44:50 -0400

    updated

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -53,7 +53,7 @@ def number_template(n=None):
     number template
     """
     if isinstance(n, int):
-        return render_template('5-number.html', n=n)
+        return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
