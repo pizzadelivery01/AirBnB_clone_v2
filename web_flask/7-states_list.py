eac62ef3aa9416d2b50dba528057b1c9fa01ae29
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-22 04:42:42 -0400

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,7 @@
 """Flask"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
@@ -23,4 +24,4 @@ def teardown_db(exception):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
