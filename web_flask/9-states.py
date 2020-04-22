27cdbbb0d39266d5c513fdc66357672a48bd1e0a
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-22 16:00:08 -0400

    adding

---
+++ web_flask/9-states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from models import *
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """
+    states
+    """
+    states = storage.all(State)
+    if state_id is not None:
+        state_id = 'State.' + state_id
+        return render_template('9-states.html', states=states, state_id=state_id)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    teardown
+    """
+    storage.close()
+
+    if __name__ == '__main__':
+        app.run()
