1bb5993da5de805fe86ed18330dcd84f8ab2d3df
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-22 15:42:32 -0400

    ''

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,7 +14,7 @@ def cities_by_states():
     """
     cities by state
     """
-    states = storage.all("State").values()
+    states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
 
