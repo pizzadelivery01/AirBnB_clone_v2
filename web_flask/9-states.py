dc411a85e615e481b345bf033b7dd918b7fd22ec
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-22 22:22:16 -0400

    hope

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,15 +10,30 @@ app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
+def list_all_states():
+    """
+    all states
+    """
+    dict_states = storage.all(State)
+    all_states = []
+    for k, v in dict_states.items():
+        all_states.append(v)
+        return render_template('9-states.html', all_states=all_states)
+
+
 @app.route('/states/<id>', strict_slashes=False)
-def states_city(id=None):
+def find_state(id):
     """
-    states_city
+    state wit id
     """
-    states = storage.all(State).values()
-    return render_template('9-states.html', states=states, id=id)
-
-
+    dict_states = storage.all(State)
+    all_states = []
+    all_states_id = []
+    for k, v in dict_states.items():
+        all_states_id.append(v.id)
+        all_states.append(v)
+        return render_template('9-states.html', all_states=all_states,
+                               all_states_id=all_states_id, id=id)
 @app.teardown_appcontext
 def teardown_db(exception):
     """
